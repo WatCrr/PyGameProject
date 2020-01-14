f195236c741d6f101d635cf221e00668c25c5c9c
Matvei <hack.number.123@gmail.com>
2020-01-14 18:04:36 +0500

    Signed-off-by: Matvei <hack.number.123@gmail.com> imported os library created load_image function, fencer class changed window size fps deleted all_sprites group created, left and right fencers added all sprites now is  being drown in game cycle

--- game.py
+++ game.py
@@ -1,17 +1,47 @@
-
+import os
 import pygame
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join("data", name)
+    image = pygame.image.load(fullname).convert()
+    if colorkey is not None:
+        if colorkey != None:
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+    return image
+
+
+class fencer(pygame.sprite.Sprite):
+    def __init__(self, group, side="left"):
+        super().__init__(group)
+        self.side = side
+        self.image = load_image(side + "_mid_stance.png", colorkey=-1)
+        self.stance = 2
+        self.rect = self.image.get_rect()
+        self.rect.y = 50
+        if side == "left":
+            self.rect.x = 150
+        if side == "right":
+            self.rect.x = 300
+
+
 pygame.init()
-size = (350, 150)
+size = (500, 150)
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 0))
-fps = (24)
+all_sprites = pygame.sprite.Group()
+left_fencer = fencer(all_sprites)
+right_fencer = fencer(all_sprites, "right")
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    all_sprites.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
