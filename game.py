9af16c0ed3a560a2c7a404c9aaf4acd9939cc9f9
Matvei <hack.number.123@gmail.com>
2020-01-15 21:45:46 +0500

    comments added

--- game.py
+++ game.py
@@ -3,24 +3,32 @@ import pygame
 
 
 def load_image(name, colorkey=None):
+    # функция загрузки изображения
+    # загружаем изображение, убираем alpha values формат записи пикселей, если таковой используется
     fullname = os.path.join("data", name)
     image = pygame.image.load(fullname).convert()
-    if colorkey is not None:
-        if colorkey != None:
-            if colorkey == -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey(colorkey)
-        else:
-            image = image.convert_alpha()
+    # устанавливаем colorkey для обрезания фона
+    if colorkey != None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
     return image
 
 
 class fencer(pygame.sprite.Sprite):
+    # класс игрока
     def __init__(self, group, side="left"):
+        # базовая инициализация
         super().__init__(group)
+        # выбор того, какой из двух игроко это будет
         self.side = side
+        # загрузка картинки игрока
         self.image = load_image(side + "_mid_stance.png", colorkey=-1)
+        # установка стойки
         self.stance = 2
+        # установка координат
         self.rect = self.image.get_rect()
         self.rect.y = 50
         if side == "left":
@@ -30,15 +38,20 @@ class fencer(pygame.sprite.Sprite):
 
 
 pygame.init()
+# создаем окно
 size = (500, 150)
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 0))
+# создаем группу всех спрайтов
 all_sprites = pygame.sprite.Group()
+# создаем спрайты игроков
 left_fencer = fencer(all_sprites)
 right_fencer = fencer(all_sprites, "right")
+# игровой цикл
 running = True
 while running:
     for event in pygame.event.get():
+        # закрытие окна
         if event.type == pygame.QUIT:
             running = False
     all_sprites.draw(screen)
