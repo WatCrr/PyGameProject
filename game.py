92d963b0c859a11068bdf19d4322a1cfa337c31c
Matvei <hack.number.123@gmail.com>
2020-01-30 17:57:06 +0500

    they can now attack + commens

--- game.py
+++ game.py
@@ -26,10 +26,10 @@ class Fencer(pygame.sprite.Sprite):
         self.side = side
         # загрузка картинки игрока
         self.image = load_image(side + "_mid_stance.png", colorkey=-1)
-        # установка стойки
+        # установка состояний
         self.stance = 2
-        # установка координат
         self.attacking = 0
+        # установка координат
         self.rect = self.image.get_rect()
         self.rect.y = 50
         if side == "left":
@@ -39,25 +39,40 @@ class Fencer(pygame.sprite.Sprite):
 
     def react(self, key):
         # реакция на нажатие клавиши
-        if (key == pygame.K_w) or (key == pygame.K_o):
-            # смена стойки на более верхнюю
-            if self.stance < 3:
-                self.stance += 1
-        if (key == pygame.K_s) or (key == pygame.K_l):
-            # смена стойки на более нижнюю
-            if self.stance > 1:
-                self.stance -= 1;
+        if self.attacking == 0:
+            if (key == pygame.K_w) or (key == pygame.K_o):
+                # смена стойки на более верхнюю
+                if self.stance < 3:
+                    self.stance += 1
+            if (key == pygame.K_s) or (key == pygame.K_l):
+                # смена стойки на более нижнюю
+                if self.stance > 1:
+                    self.stance -= 1
+            if (key == pygame.K_LALT) or (key == pygame.K_RALT):
+                self.attacking = 24
 
 
     def next_state(self):
         # обноление состояния
-        if self.stance == 1:
-            self.image = load_image(self.side + "_down_stance.png", colorkey=-1)
-        if self.stance == 2:
-            self.image = load_image(self.side + "_mid_stance.png", colorkey=-1)
-        if self.stance == 3:
-            self.image = load_image(self.side + "_up_stance.png", colorkey=-1)
-
+        if self.attacking == 0:
+            if self.stance == 1:
+                self.image = load_image(self.side + "_down_stance.png", colorkey=-1)
+            if self.stance == 2:
+                self.image = load_image(self.side + "_mid_stance.png", colorkey=-1)
+            if self.stance == 3:
+                self.image = load_image(self.side + "_up_stance.png", colorkey=-1)
+        else:
+            if self.stance == 1:
+                self.image = load_image(self.side + "_down_attack.png", colorkey=-1).\
+                    subsurface(pygame.Rect(50 - self.attacking // 19 * 50, 0, 50, 50))
+            if self.stance == 2:
+                self.image = load_image(self.side + "_mid_attack.png", colorkey=-1).\
+                    subsurface(pygame.Rect(50 - self.attacking // 19 * 50, 0, 50, 50))
+            if self.stance == 3:
+                self.image = load_image(self.side + "_up_attack.png", colorkey=-1).\
+                    subsurface(pygame.Rect(50 - self.attacking // 19 * 50, 0, 50, 50))
+            self. attacking -= 1
+           
 
 pygame.init()
 # создаем окно
@@ -70,6 +85,9 @@ all_sprites = pygame.sprite.Group()
 left_fencer = Fencer(all_sprites)
 right_fencer = Fencer(all_sprites, "right")
 # игровой цикл
+# таймер
+clock = pygame.time.Clock()
+fps = 24
 running = True
 while running:
     for event in pygame.event.get():
@@ -77,9 +95,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if (event.key == pygame.K_o) or (event.key == pygame.K_l):
+            if (event.key == pygame.K_o) or (event.key == pygame.K_l) or (event.key == pygame.K_RALT):
                 right_fencer.react(event.key)
-            if (event.key == pygame.K_w) or (event.key == pygame.K_s):
+            if (event.key == pygame.K_w) or (event.key == pygame.K_s) or (event.key == pygame.K_LALT):
                 left_fencer.react(event.key)
     right_fencer.next_state()
     left_fencer.next_state()
@@ -87,5 +105,6 @@ while running:
     screen.fill((120, 60, 30))
     all_sprites.draw(screen)
     pygame.display.flip()
+    clock.tick(fps)
 # закрыаем окно
 pygame.quit()
